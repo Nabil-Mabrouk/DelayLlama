a539c86feffd9d30f14e5f1711e2d9a42d8f1a98
Nabil-Mabrouk <nabil.mabrouk.ai@gmail.com>
2023-08-26 01:02:25 +0100

    working

--- src/database.py
+++ src/database.py
@@ -1,24 +1,24 @@
 import sqlite3
 
 class Database:
-    def __init__(self, db_url='sqlite:///indicator_data.db'):
+    def __init__(self):
         """
         Initialize a Database instance to interact with an SQL database.
 
         Args:
             db_url (str): The database URL. Defaults to SQLite in-memory database.
         """
-        self.db_url = db_url
+        self.db_url = ""
         self.connection = None
 
     def connect(self):
         """Connect to the database."""
-        self.connection = sqlite3.connect(self.db_url)
+        #self.connection = sqlite3.connect(self.db_url)
 
     def disconnect(self):
         """Disconnect from the database."""
-        if self.connection:
-            self.connection.close()
+        #if self.connection:
+        #    self.connection.close()
 
     def get_available_indicators(self):
         """
@@ -27,11 +27,12 @@ class Database:
         Returns:
             list: List of indicator names.
         """
-        self.connect()
-        cursor = self.connection.cursor()
-        cursor.execute("SELECT DISTINCT indicator_name FROM indicator_data")
-        indicator_names = [row[0] for row in cursor.fetchall()]
-        self.disconnect()
+        #self.connect()
+        #cursor = self.connection.cursor()
+        #cursor.execute("SELECT DISTINCT indicator_name FROM indicator_data")
+        #indicator_names = [row[0] for row in cursor.fetchall()]
+        #self.disconnect()
+        indicator_names=["SMA", "volatility"]
         return indicator_names
 
     def retrieve_indicator_data(self, indicator_name):
@@ -44,11 +45,12 @@ class Database:
         Returns:
             list: Historical data for the specified indicator.
         """
-        self.connect()
-        cursor = self.connection.cursor()
-        cursor.execute("SELECT data FROM indicator_data WHERE indicator_name = ?", (indicator_name,))
-        data = [row[0] for row in cursor.fetchall()]
-        self.disconnect()
+        #self.connect()
+        #cursor = self.connection.cursor()
+        #cursor.execute("SELECT data FROM indicator_data WHERE indicator_name = ?", (indicator_name,))
+        #data = [row[0] for row in cursor.fetchall()]
+        #self.disconnect()
+        data=[1, 2, 3,4,5]
         return data
 
 # Example usage:
